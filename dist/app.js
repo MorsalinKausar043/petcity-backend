7a1b09367209d338be797c4d657dcc3762fc940a
Morsalin <morsalinkausar043@gmail.com>
2023-06-25 19:25:16 +0600

    done

--- dist/app.js
+++ dist/app.js
@@ -6,12 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-// import route 
+// import route
 const job_route_1 = __importDefault(require("./app/modules/job/job.route"));
+const apply_route_1 = __importDefault(require("./app/modules/apply/apply.route"));
 // use middleware
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-// use custom url 
+// use custom url
 app.use("/job", job_route_1.default);
-app.get("/", (req, res, next) => res.status(201).send({ status: true, massage: "Hello World This is Pet-City Api!" }));
+app.use("/apply", apply_route_1.default);
+app.get("/", (req, res, next) => res
+    .status(201)
+    .send({ status: true, massage: "Hello World This is Pet-City Api!" }));
 exports.default = app;
---
+++ src/app/modules/apply/apply.interface.ts
@@ -0,0 +1,12 @@
+export type IApply = {
+  jobId: string;
+  name: {
+    firstName: string;
+    lastName: string;
+  };
+  email: string;
+  phone: number;
+  address: string;
+  description: string;
+  status:boolean;
+};
